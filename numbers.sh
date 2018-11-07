1ee69a8ccc7dfc8fc09f3ee94b33aeb34f3e9201
Nicholas Mirchandani <nmirchandani@chapman.edu>
2018-11-07 12:41:04 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -2,23 +2,24 @@
 #numbers.sh
 #Nicholas Mirchandani
 
+myInt=1
+
 echo "Enter a positive integer: "
 read INTEGER
-myInt = 1
 
 if ((INTEGER < 1))
 then
 	echo Y u do dis
 else
-	myInt = 1
-	while ((myInt < INTEGER))
+	while ((myInt < $((INTEGER+1))))
 	do
-		if $((myInt % 2)) -eq 0
+		if [ $((myInt % 2)) -eq 0 ]
 		then
 			echo $myInt Even
+			myInt=$((myInt+1))
 		else
 			echo $myInt Odd
+			myInt=$((myInt+1))
 		fi
-		myInt = $((i+1))
 	done
 fi
